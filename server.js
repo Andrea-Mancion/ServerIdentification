4114d964f5e76d94920d2d26e018602a9bbdf278
Andrea-Mancion <andrea.mancion@epitech.eu>
2024-03-16 21:09:59 +0100

    FEAT(GetUser): can get the username and password

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    console.log(`Username: ${username} Password: ${password}`);
+    res.send('User registered');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
